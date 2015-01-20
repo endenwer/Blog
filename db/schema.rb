5235be9c865b4261725f7c8cf2c5b7d58d62245e
Stepan Lusnikov <endenwer@gmail.com>
2015-01-20 23:10:23 +0300

    Add a Hub model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120194925) do
+ActiveRecord::Schema.define(version: 20150120200901) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150120194925) do
     t.integer  "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "hubs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: :cascade do |t|

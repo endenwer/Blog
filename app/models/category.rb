d6fe9ed16129d5122d71505d376c6ce08e8e7512
Stepan Lusnikov <endenwer@gmail.com>
2015-01-21 22:20:26 +0300

    Model test for Comment is pass

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+  has_many :posts
+
+  validates :name, presence: true
+  validates :description, presence: true
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @category = Category.new(name: 'Category name', description: 'Category description') }
+
+  subject { @category }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:description) }
+  it { should respond_to(:posts) }
+
+  it { should be_valid }
+
+  describe 'with blank name' do
+    before { @category.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'with blank description' do
+    before { @category.description = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'post associated' do
+    before { @category.save }
+    let(:post) { FactoryGirl.create(:post, category_id: @category.id) }
+
+    it 'should have a post' do
+      expect(@category.posts).to include(post)
+    end
+  end
+
 end

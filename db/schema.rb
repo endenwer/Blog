ac28528f2748fd5533fff71737ff07520bf4f5e2
Lusnikov Stepan <endenwer@gmail.com>
2015-05-26 21:26:31 +0300

    Change a name of gem Bcrypt in Gemfile.

--- db/schema.rb
+++ db/schema.rb
@@ -58,23 +58,6 @@ ActiveRecord::Schema.define(version: 20150225214238) do
     t.integer  "category_id"
   end
 
-  create_table "redactor_assets", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "redactor_assets", ["assetable_type", "assetable_id"], name: "idx_redactor_assetable"
-  add_index "redactor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_redactor_assetable_type"
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false

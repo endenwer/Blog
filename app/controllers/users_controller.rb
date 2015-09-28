e686e933f185081487611cbad677027d7f76a1dc
Stepan Lusnikov <endenwer@gmail.com>
2015-09-28 11:07:51 +0300

    Output posts of the users on the profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.paginate(page: params[:page], per_page: 5)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
 

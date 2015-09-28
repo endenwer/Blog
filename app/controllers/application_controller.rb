984b85a7aa138d983113e125605fcad317a9bcf3
Stepan Lusnikov <endenwer@gmail.com>
2015-09-28 10:30:57 +0300

    Output categories in the menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :categories
   add_flash_types :danger, :success, :info
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :login
+  end
+
+  def categories
+    @categories = Category.all
   end
 end
